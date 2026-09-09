370fc241e5794da5499ec45814462638e4e5d8f5
Zadu <jartetab@unicartagena.edu.co>
2026-09-09 14:17:36 -0500

    Configuración inicial del proyecto de Spring Boot

---
+++ src/main/java/com/universidad/UniversidadSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.universidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversidadSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversidadSpringBootApplication.class, args);
+    }
+
+}
